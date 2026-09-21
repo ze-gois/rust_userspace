af703005ec14f1012ed33fcbffcd9100d6e35851
Zé Gois <2962558+ze-gois@users.noreply.github.com>
2026-09-21 19:56:51 -0300

    refactor: keep architecture syscall results raw

--- src/target/architecture/x86/bit64/syscall/syscall0.rs
+++ src/target/architecture/x86/bit64/syscall/syscall0.rs
@@ -1,70 +1,13 @@
-use super::result::*;
-
 #[inline(always)]
-pub fn syscall0(n: usize) -> crate::Result {
-    let syscall_return: usize;
+pub fn syscall0(number: usize) -> usize {
+    let result: usize;
     unsafe {
         core::arch::asm!(
             "syscall",
-            inlateout("rax") n => syscall_return,
+            inlateout("rax") number => result,
             out("rcx") _,
             out("r11") _,
         );
     }
-    handle_result(syscall_return)
-}
-
-pub mod ok {
-    ample::result!(
-        Ok;
-        "Architecture syscall Ok";
-        usize;
-        [
-            [0; SYSCALL0_OK; Default; usize; "ZE"; "Entry to ze"],
-        ]
-    );
-
-    impl Ok {
-        pub fn from_no(no: usize) -> Self {
-            Ok::Default(no)
-        }
-    }
-}
-
-pub mod error {
-    ample::result!(
-        Error;
-        "Architecture syscall Error";
-        usize;
-        [
-            [0; SYSCALL0_ERROR; Default; usize; "ZE"; "Entry to ze"],
-        ]
-    );
-
-    impl Error {
-        pub fn from_no(no: usize) -> Self {
-            Error::Default(no)
-        }
-    }
-}
-
-pub use error::Error;
-pub use ok::Ok;
-
-pub type Result = core::result::Result<Ok, Error>;
-
-pub fn handle_result(result: usize) -> crate::Result {
-    if (result as isize) < 0 {
-        core::result::Result::Err(crate::Error::Target(crate::target::Error::Architecture(
-            crate::target::architecture::Error::Syscall(
-                crate::target::architecture::x86::bit64::syscall::Error::Syscall0(Error::Default(result)),
-            ),
-        )))
-    } else {
-        core::result::Result::Ok(crate::Ok::Target(crate::target::Ok::Architecture(
-            crate::target::architecture::Ok::Syscall(
-                crate::target::architecture::x86::bit64::syscall::Ok::Syscall0(Ok::Default(result)),
-            ),
-        )))
-    }
+    result
 }
