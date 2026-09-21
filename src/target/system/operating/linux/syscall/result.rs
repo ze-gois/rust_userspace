917a3c4b6493ebbee9e2cf8064586fcbab9c368f
Zé Gois <2962558+ze-gois@users.noreply.github.com>
2026-09-21 20:36:11 -0300

    refactor: centralize Linux syscall outcomes

--- src/target/system/operating/linux/syscall/result.rs
+++ src/target/system/operating/linux/syscall/result.rs
@@ -1,19 +1,19 @@
 pub mod ok {
     ample::result!( Ok; "Linux syscall Ok"; usize; [
         [0; OK;         Default; usize; "Ok"; "All good"],
-        [2; ERR_CLOSE;  Close;    super::super::close::Ok;        "close";     "E_CLOSE" ],
-        [3; ERR_LSEEK;  Lseek;    super::super::lseek::Ok;        "lseek";     "E_LSEEK"],
-        [4; ERR_MMAP;  Mmap;     super::super::mmap::Ok;          "mmap";      "E_MMAP"],
-        [5; ERR_MPROTECT;  Mprotect; super::super::mprotect::Ok;  "mprotect";  "E_MPROTECT"],
-        [6; ERR_MUNMAP;  Munmap;   super::super::munmap::Ok;      "munmap";    "E_MUNMAP"],
+        [2; ERR_CLOSE;  Close;    super::super::Success;        "close";     "E_CLOSE" ],
+        [3; ERR_LSEEK;  Lseek;    super::super::Success;        "lseek";     "E_LSEEK"],
+        [4; ERR_MMAP;  Mmap;     super::super::Success;          "mmap";      "E_MMAP"],
+        [5; ERR_MPROTECT;  Mprotect; super::super::Success;  "mprotect";  "E_MPROTECT"],
+        [6; ERR_MUNMAP;  Munmap;   super::super::Success;      "munmap";    "E_MUNMAP"],
         [7; ERR_OPEN;  Open;     super::super::Success;          "open";      "E_OPEN"],
         [14; ERR_OPENAT; Openat; super::super::Success; "openat"; "E_OPENAT"],
-        [8; ERR_READ;  Read;     super::super::read::Ok;          "read";      "E_READ"],
-        [9; ERR_WRITE;  Write;    super::super::write::Ok;        "write";     "E_WRITE" ],
-        [10; ERR_FSTAT; Fstat;    super::super::fstat::Ok;        "fstat";     "E_FSTAT"],
-        [11; ERR_FORK;  Fork;     super::super::fork::Ok;         "fork";      "E_FORK" ],
-        [12; ERR_EXECVE; Execve;    super::super::execve::Ok;       "execve";    "E_EXECVE"],
-        [13; ERR_GETRANDOM; Getrandom; super::super::getrandom::Ok; "getrandom"; "E_GETRANDOM"]
+        [8; ERR_READ;  Read;     super::super::Success;          "read";      "E_READ"],
+        [9; ERR_WRITE;  Write;    super::super::Success;        "write";     "E_WRITE" ],
+        [10; ERR_FSTAT; Fstat;    super::super::Success;        "fstat";     "E_FSTAT"],
+        [11; ERR_FORK;  Fork;     super::super::Success;         "fork";      "E_FORK" ],
+        [12; ERR_EXECVE; Execve;    super::super::Success;       "execve";    "E_EXECVE"],
+        [13; ERR_GETRANDOM; Getrandom; super::super::Success; "getrandom"; "E_GETRANDOM"]
     ]);
 
     impl Ok {
@@ -26,19 +26,19 @@ pub mod ok {
 pub mod error {
     ample::result!(Error; "Linux syscall Error"; usize; [
         [1; ERROR;      Default; usize; "Error"; "Something wicked this way comes"],
-        [2; ERR_CLOSE;  Close;    super::super::close::Error;        "close";     "E_CLOSE" ],
-        [3; ERR_LSEEK;  Lseek;    super::super::lseek::Error;        "lseek";     "E_LSEEK"],
-        [4; ERR_MMAP;  Mmap;     super::super::mmap::Error;          "mmap";      "E_MMAP"],
-        [5; ERR_MPROTECT;  Mprotect; super::super::mprotect::Error;  "mprotect";  "E_MPROTECT"],
-        [6; ERR_MUNMAP;  Munmap;   super::super::munmap::Error;      "munmap";    "E_MUNMAP"],
+        [2; ERR_CLOSE;  Close;    super::super::Failure;        "close";     "E_CLOSE" ],
+        [3; ERR_LSEEK;  Lseek;    super::super::Failure;        "lseek";     "E_LSEEK"],
+        [4; ERR_MMAP;  Mmap;     super::super::Failure;          "mmap";      "E_MMAP"],
+        [5; ERR_MPROTECT;  Mprotect; super::super::Failure;  "mprotect";  "E_MPROTECT"],
+        [6; ERR_MUNMAP;  Munmap;   super::super::Failure;      "munmap";    "E_MUNMAP"],
         [7; ERR_OPEN;  Open;     super::super::Failure;          "open";      "E_OPEN"],
         [14; ERR_OPENAT; Openat; super::super::Failure; "openat"; "E_OPENAT"],
-        [8; ERR_READ;  Read;     super::super::read::Error;          "read";      "E_READ"],
-        [9; ERR_WRITE;  Write;    super::super::write::Error;        "write";     "E_WRITE"],
-        [10; ERR_FSTAT; Fstat;    super::super::fstat::Error;        "fstat";     "E_FSTAT"],
-        [11; ERR_FORK;  Fork;     super::super::fork::Error;         "fork";      "E_FORK" ],
-        [12; ERR_EXECVE;  Execve;    super::super::execve::Error;       "execve";    "E_EXECVE" ],
-        [13; ERR_GETRANDOM; Getrandom; super::super::getrandom::Error; "getrandom"; "E_GETRANDOM"]
+        [8; ERR_READ;  Read;     super::super::Failure;          "read";      "E_READ"],
+        [9; ERR_WRITE;  Write;    super::super::Failure;        "write";     "E_WRITE"],
+        [10; ERR_FSTAT; Fstat;    super::super::Failure;        "fstat";     "E_FSTAT"],
+        [11; ERR_FORK;  Fork;     super::super::Failure;         "fork";      "E_FORK" ],
+        [12; ERR_EXECVE;  Execve;    super::super::Failure;       "execve";    "E_EXECVE" ],
+        [13; ERR_GETRANDOM; Getrandom; super::super::Failure; "getrandom"; "E_GETRANDOM"]
     ]);
 
     impl Error {
