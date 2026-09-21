484976462afa7584ac0af4e0f7cba7a78a48766f
Zé Gois <2962558+ze-gois@users.noreply.github.com>
2026-09-21 19:53:03 -0300

    refactor: call concrete architecture syscall mechanism

--- src/target/system/operating/linux/syscall/exit.rs
+++ src/target/system/operating/linux/syscall/exit.rs
@@ -1,10 +1,10 @@
-use crate::target::architecture::Architecture;
+use crate::target::architecture::x86::bit64::syscall;
 
 #[cfg(target_arch = "x86_64")]
 pub const NUMBER: usize = super::number::x86::bit64::EXIT;
 
 pub fn exit(status_code: i32) -> ! {
-    let _ = Architecture::syscall1(NUMBER, status_code as usize);
+    let _ = syscall::syscall1(NUMBER, status_code as usize);
 
     loop {
         core::hint::spin_loop();
