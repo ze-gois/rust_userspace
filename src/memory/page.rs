0fb7ed5c7d3d6223276e3e987a1096873392672e
Zé Gois <2962558+ze-gois@users.noreply.github.com>
2026-09-21 19:54:48 -0300

    refactor: name base page size explicitly

--- src/memory/page.rs
+++ src/memory/page.rs
@@ -1,6 +1,6 @@
-pub use crate::target::architecture::page::SIZE;
+pub use crate::target::architecture::page::BASE_BASE_SIZE;
 
-const MASK: usize = !(SIZE - 1);
+const MASK: usize = !(BASE_SIZE - 1);
 
 #[inline]
 pub const fn align_down(address: usize) -> usize {
@@ -9,7 +9,7 @@ pub const fn align_down(address: usize) -> usize {
 
 #[inline]
 pub const fn align_up(address: usize) -> Option<usize> {
-    match address.checked_add(SIZE - 1) {
+    match address.checked_add(BASE_SIZE - 1) {
         Some(address) => Some(align_down(address)),
         None => None,
     }
