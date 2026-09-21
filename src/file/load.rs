8f964d3ff3e69b19b2e01bad2b6d910e0b259d7c
Zé Gois <2962558+ze-gois@users.noreply.github.com>
2026-09-21 15:38:10 -0300

    refactor: terminate load path through Allocating

--- src/file/load.rs
+++ src/file/load.rs
@@ -2,16 +2,10 @@ use crate::memory::heap::Allocator;
 use crate::target::os::syscall;
 
 pub fn load(filepath: &str) -> Option<(isize, syscall::fstat::Stat, *const u8)> {
-    use ample::traits::AllocatableResult;
-    let filepath =
-        ample::string::terminate::<crate::Origin, crate::Origin, crate::memory::heap::Allocator>(
-            filepath,
-        );
-
-    let filepath = match filepath {
-        core::result::Result::Ok(a) => a.as_ptr(),
-        _ => return None,
-    };
+    let filepath = ample::string::terminate::<Allocator>(filepath);
+    if filepath.is_null() {
+        return None;
+    }
 
     let license_mapping;
     'opening: loop {
