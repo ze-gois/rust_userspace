6fad9bfe569d0ff9f145d339a26846d66b05edb0
Zé Gois <2962558+ze-gois@users.noreply.github.com>
2026-09-21 20:39:31 -0300

    refactor: make result envelopes structural

--- src/target/result.rs
+++ src/target/result.rs
@@ -1,50 +1,26 @@
 pub mod ok {
     ample::result!(
         Ok;
-        "Target Ok";
+        "Target success";
         usize;
         [
-            [1; TARGET_DEFAULT_OK; Default; usize; "Target"; "Target result"],
-            [2; TARGET_INFO_OK; Info; usize; "Target"; "Target result"],
-            [3; TARGET_OPERATING_SYSTEM_OK; OperatingSystem; crate::target::system::operating::Ok; "Target"; "Target result"],
+            [0; TARGET_OPERATING_SYSTEM_SUCCESS; OperatingSystem; crate::target::system::operating::Ok; "operating system"; "Operating-system target success"]
         ]
     );
-
-    impl Ok {
-        pub fn from_no(no: usize) -> Self {
-            Ok::Default(no)
-        }
-    }
 }
 
 pub mod error {
     ample::result!(
         Error;
-        "Target Error";
+        "Target failure";
         usize;
         [
-            [1; TARGET_DEFAULT_ERROR; Default; usize; "Target"; "Target result"],
-            [2; TARGET_INFO_ERROR; Info; usize; "Target"; "Target result"],
-            [3; TARGET_OPERATING_SYSTEM_ERROR; OperatingSystem; crate::target::system::operating::Error; "Target"; "Target result"],
+            [0; TARGET_OPERATING_SYSTEM_FAILURE; OperatingSystem; crate::target::system::operating::Error; "operating system"; "Operating-system target failure"]
         ]
     );
-
-    impl Error {
-        pub fn from_no(no: usize) -> Self {
-            Error::Default(no)
-        }
-    }
 }
 
 pub use error::Error;
 pub use ok::Ok;
 
 pub type Result = core::result::Result<Ok, Error>;
-
-// pub fn handle_result(result: usize) -> Result {
-//     if (result as isize) < 0 {
-//         Err(Error::from_no(result))
-//     } else {
-//         Ok(Ok::from_no(result))
-//     }
-// }
