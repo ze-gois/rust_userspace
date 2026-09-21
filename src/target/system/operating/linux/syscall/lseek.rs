931726a7496f12904f6b16ba4f56953b76fbb3aa
Zé Gois <2962558+ze-gois@users.noreply.github.com>
2026-09-21 19:15:06 -0300

    refactor: use canonical Linux paths

--- src/target/system/operating/linux/syscall/lseek.rs
+++ src/target/system/operating/linux/syscall/lseek.rs
@@ -52,12 +52,12 @@ pub fn handle_result(result: crate::Result) -> crate::Result {
                 ),
             ),
         ))) => core::result::Result::Ok(crate::Ok::Target(crate::target::Ok::OperatingSystem(
-            crate::target::os::Ok::Syscall(crate::target::os::syscall::Ok::LSeek(
-                crate::target::os::syscall::lseek::Ok::Default(m),
+            crate::target::system::operating::linux::Ok::Syscall(crate::target::system::operating::linux::syscall::Ok::LSeek(
+                crate::target::system::operating::linux::syscall::lseek::Ok::Default(m),
             )),
         ))),
         _ => core::result::Result::Err(crate::Error::Target(crate::target::Error::OperatingSystem(
-            crate::target::os::Error::Syscall(crate::target::os::syscall::Error::LSeek(
+            crate::target::system::operating::linux::Error::Syscall(crate::target::system::operating::linux::syscall::Error::LSeek(
                 Error::Default(3),
             )),
         ))),
