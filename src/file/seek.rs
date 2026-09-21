c13d7816c25eefd2b92524c3915c9239d0ad37df
Zé Gois <2962558+ze-gois@users.noreply.github.com>
2026-09-21 18:30:52 -0300

    refactor: seek with Whence

--- src/file/seek.rs
+++ src/file/seek.rs
@@ -4,7 +4,7 @@ pub fn seek(file_descriptor: isize, offset: i64) -> usize {
     match syscall::lseek(
         file_descriptor as i32,
         offset,
-        syscall::lseek::Flag::SET.to(),
+        syscall::lseek::Whence::Set.to(),
     ) {
         core::result::Result::Ok(crate::Ok::Target(crate::target::Ok::OperatingSystem(
             crate::target::os::Ok::Syscall(crate::target::os::syscall::Ok::LSeek(
