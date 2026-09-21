20be05e7fe6659f1a08130cd594e65cefda6fd6e
Zé Gois <2962558+ze-gois@users.noreply.github.com>
2026-09-21 19:54:45 -0300

    refactor: name entry stack pointer explicitly

--- src/entry.rs
+++ src/entry.rs
@@ -2,6 +2,6 @@
 #![no_main]
 
 #[unsafe(no_mangle)]
-pub extern "C" fn entry(_stack_pointer: userspace::target::arch::PointerType) -> ! {
-    userspace::target::os::syscall::exit(0)
+pub extern "C" fn entry(_stack_pointer: userspace::target::arch::StackPointer) -> ! {
+    userspace::target::os::linux::syscall::exit(0)
 }
