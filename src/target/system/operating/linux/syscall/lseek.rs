36365d7267c5e4392ecdbc7f2aa09d5a45b7112a
Zé Gois <2962558+ze-gois@users.noreply.github.com>
2026-09-21 20:26:15 -0300

    refactor: type lseek origin selector

--- src/target/system/operating/linux/syscall/lseek.rs
+++ src/target/system/operating/linux/syscall/lseek.rs
@@ -7,8 +7,15 @@ pub use whence::Whence;
 pub const NUMBER: usize = super::number::x86::bit64::LSEEK;
 
 #[inline(always)]
-pub fn lseek(file_descriptor: i32, offset: i64, whence: i32) -> crate::Result {
-    let raw_return = unsafe { syscall::syscall3(NUMBER, file_descriptor as usize, offset as usize, whence as usize);
+pub fn lseek(file_descriptor: i32, offset: i64, whence: Whence) -> crate::Result {
+    let raw_return = unsafe {
+        syscall::syscall3(
+            NUMBER,
+            file_descriptor as usize,
+            offset as usize,
+            whence.raw() as usize,
+        )
+    };
 
     handle_result(raw_return)
 }
