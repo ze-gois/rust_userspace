e040274c04210258d1c04e2d76310dff71f2eb56
Zé Gois <2962558+ze-gois@users.noreply.github.com>
2026-09-21 18:24:38 -0300

    refactor: make Linux target conditional

--- src/target/system/operating.rs
+++ src/target/system/operating.rs
@@ -7,11 +7,6 @@ pub mod linux;
 #[cfg(target_os = "linux")]
 pub use linux::*;
 
-#[cfg(not(target_os = "linux"))]
-pub mod linux;
-#[cfg(not(target_os = "linux"))]
-pub use linux::*;
-
 #[derive(Debug, Clone, Copy, Default)]
 pub struct OperatingSystem;
 
