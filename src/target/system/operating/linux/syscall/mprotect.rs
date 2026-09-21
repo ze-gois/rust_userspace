d891d1904fee7c209433f0e44fbcdc31fed45caf
Zé Gois <2962558+ze-gois@users.noreply.github.com>
2026-09-21 19:32:07 -0300

    refactor: keep Linux architecture boundary explicit

--- src/target/system/operating/linux/syscall/mprotect.rs
+++ src/target/system/operating/linux/syscall/mprotect.rs
@@ -45,8 +45,8 @@ pub fn handle_result(result: crate::Result) -> crate::Result {
     match result {
         crate::Result::Ok(crate::Ok::Target(crate::target::Ok::Architecture(
             crate::target::architecture::Ok::Syscall(
-                crate::target::architecture::syscall::Ok::Syscall3(
-                    crate::target::architecture::syscall::syscall3::Ok::Default(m),
+                crate::target::architecture::x86::bit64::syscall::Ok::Syscall3(
+                    crate::target::architecture::x86::bit64::syscall::syscall3::Ok::Default(m),
                 ),
             ),
         ))) => core::result::Result::Ok(crate::Ok::Target(crate::target::Ok::OperatingSystem(
