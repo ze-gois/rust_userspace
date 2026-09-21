dc6f928f8ee4630ccbb3a8a0af3fa849a45e70ee
Zé Gois <2962558+ze-gois@users.noreply.github.com>
2026-09-21 15:35:11 -0300

    fix: preserve mmap base in global allocator

--- src/memory/alloc.rs
+++ src/memory/alloc.rs
@@ -1,22 +1,16 @@
 use crate::memory::heap::Allocator;
-use ample::traits::Allocatable;
+use ample::traits::Allocating;
 use core::alloc::{GlobalAlloc, Layout};
 
 struct BlergAlloc;
 
 unsafe impl GlobalAlloc for BlergAlloc {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        match Allocator::allocate(layout.size() + layout.align()) {
-            core::result::Result::Ok(crate::Ok::Memory(crate::memory::Ok::HeapAllocate(m))) => unsafe {
-                let m = m as *mut u8;
-                m.add(layout.align())
-            },
-            _ => core::ptr::null_mut(),
-        }
+        <Allocator as Allocating>::allocate(layout)
     }
 
-    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
-        let _ = Allocator::deallocate(ptr as *mut Allocator, layout.size() + layout.align());
+    unsafe fn dealloc(&self, pointer: *mut u8, layout: Layout) {
+        let _ = unsafe { <Allocator as Allocating>::deallocate(pointer, layout) };
     }
 }
 
