d221424f8af90e274d4df186c68aaee12ba8d6fc
Zé Gois <2962558+ze-gois@users.noreply.github.com>
2026-09-21 20:01:42 -0300

    refactor: use canonical target paths internally

--- src/entry.rs
+++ src/entry.rs
@@ -2,6 +2,8 @@
 #![no_main]
 
 #[unsafe(no_mangle)]
-pub extern "C" fn entry(_stack_pointer: userspace::target::arch::StackPointer) -> ! {
-    userspace::target::os::linux::syscall::exit(0)
+pub extern "C" fn entry(
+    _stack_pointer: userspace::target::architecture::StackPointer,
+) -> ! {
+    userspace::target::system::operating::linux::syscall::exit(0)
 }
