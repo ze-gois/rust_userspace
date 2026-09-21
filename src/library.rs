3f48b58e689c652219d4b88f99fa165c999c82f1
Zé Gois <2962558+ze-gois@users.noreply.github.com>
2026-09-21 15:28:22 -0300

    refactor: adopt representation size terminology

--- src/library.rs
+++ src/library.rs
@@ -24,11 +24,10 @@ pub use result::{Error, Ok, Result};
 ample::trait_implement_primitives!();
 
 // impl ample::traits::Bytes<crate::Origin, crate::Origin> for u8 {
-//     const BYTES_SIZE: usize = 1;
-//     const BYTES_ALIGN: usize = 1;
+//     const REPRESENTATION_SIZE: usize = 1;
 
 //     fn from_bytes(
-//         bytes: [u8; <Self as crate::traits::Bytes<Origin, Origin>>::BYTES_SIZE],
+//         bytes: [u8; <Self as crate::traits::Bytes<Origin, Origin>>::REPRESENTATION_SIZE],
 //         _endianness: bool,
 //     ) -> Self {
 //         bytes[0]
@@ -37,7 +36,7 @@ ample::trait_implement_primitives!();
 //     fn to_bytes(
 //         &self,
 //         _endianness: bool,
-//     ) -> [u8; <Self as crate::traits::Bytes<Origin, Origin>>::BYTES_SIZE] {
+//     ) -> [u8; <Self as crate::traits::Bytes<Origin, Origin>>::REPRESENTATION_SIZE] {
 //         [*self]
 //     }
 // }
@@ -45,17 +44,16 @@ ample::trait_implement_primitives!();
 // ample::trait_bytes_naturalize!(ample, crate);
 
 // impl<A: ample::traits::Bytes<ample::Origin>> crate::traits::Bytes<Origin, Destination> for A {
-//     const BYTES_SIZE: usize = A::BYTES_SIZE;
-//     const BYTES_ALIGN: usize = A::BYTES_ALIGN;
+//     const REPRESENTATION_SIZE: usize = A::REPRESENTATION_SIZE;
 
 //     fn from_bytes(
-//         bytes: [u8; <Self as crate::traits::Bytes<Origin, Destination>>::BYTES_SIZE],
+//         bytes: [u8; <Self as crate::traits::Bytes<Origin, Destination>>::REPRESENTATION_SIZE],
 //         endianness: bool,
 //     ) -> Self
 //     where
-//         [u8; <A as ample::traits::Bytes<ample::Origin>>::BYTES_SIZE]:,
+//         [u8; <A as ample::traits::Bytes<ample::Origin>>::REPRESENTATION_SIZE]:,
 //     {
-//         let mut ample_bytes = [0u8; <A as ample::traits::Bytes<ample::Origin>>::BYTES_SIZE];
+//         let mut ample_bytes = [0u8; <A as ample::traits::Bytes<ample::Origin>>::REPRESENTATION_SIZE];
 //         ample_bytes.copy_from_slice(&bytes);
 //         <A as ample::traits::Bytes<ample::Origin>>::from_bytes(ample_bytes, endianness)
 //     }
@@ -63,7 +61,7 @@ ample::trait_implement_primitives!();
 //     fn to_bytes(
 //         &self,
 //         endianness: bool,
-//     ) -> [u8; <Self as crate::traits::Bytes<Origin, Destination>>::BYTES_SIZE] {
+//     ) -> [u8; <Self as crate::traits::Bytes<Origin, Destination>>::REPRESENTATION_SIZE] {
 //         <A as ample::traits::Bytes<ample::Origin>>::to_bytes(self, endianness)
 //     }
 // }
