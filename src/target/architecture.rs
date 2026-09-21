9a609bc824251cf2a558f47c58b61b916ec3d9a0
Zé Gois <2962558+ze-gois@users.noreply.github.com>
2026-09-21 18:24:34 -0300

    refactor: name architecture explicitly

--- src/target/architecture.rs
+++ src/target/architecture.rs
@@ -1,24 +1,27 @@
+pub mod x86;
+
 #[cfg(target_arch = "x86_64")]
-pub mod x86_64;
-#[cfg(target_arch = "x86_64")]
-pub use x86_64::*;
+pub use x86::bit64::*;
 
 pub mod macros;
 pub mod traits;
 
-ample::r#struct!(
-    pub struct Arch {}
-);
+#[derive(Debug, Clone, Copy, Default)]
+pub struct Architecture;
+
+pub type Arch = Architecture;
 
 impl core::ops::Sub for Pointer {
     type Output = usize;
+
     fn sub(self, rhs: Self) -> Self::Output {
-        self.0 as usize - rhs.0 as usize // diferença em bytes
+        self.0 as usize - rhs.0 as usize
     }
 }
 
 impl core::ops::Add<usize> for Pointer {
     type Output = Pointer;
+
     fn add(self, offset: usize) -> Self::Output {
         Pointer(unsafe { self.0.add(offset) })
     }
