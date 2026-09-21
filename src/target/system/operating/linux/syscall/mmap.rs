0873294969c15bcbdfab6d6afcc60ddc28eaeb53
Zé Gois <2962558+ze-gois@users.noreply.github.com>
2026-09-21 18:26:52 -0300

    refactor: use explicit target result domains

--- src/target/system/operating/linux/syscall/mmap.rs
+++ src/target/system/operating/linux/syscall/mmap.rs
@@ -57,29 +57,29 @@ pub type Result = core::result::Result<Ok, Error>;
 pub fn handle_result(result: crate::Result) -> crate::Result {
     // Err(crate::Error::Default(1))
     match result {
-        crate::Result::Ok(crate::Ok::Target(crate::target::Ok::Arch(
+        crate::Result::Ok(crate::Ok::Target(crate::target::Ok::Architecture(
             crate::target::arch::Ok::X86_64Syscall(
                 crate::target::arch::syscall::Ok::X86_64Syscall6(
                     crate::target::arch::syscall::syscall6::Ok::Default(m),
                 ),
             ),
-        ))) => core::result::Result::Ok(crate::Ok::Target(crate::target::Ok::Os(
+        ))) => core::result::Result::Ok(crate::Ok::Target(crate::target::Ok::OperatingSystem(
             crate::target::os::Ok::Syscall(crate::target::os::syscall::Ok::MMap(
                 crate::target::os::syscall::mmap::Ok::Default(m),
             )),
         ))),
-        crate::Result::Err(crate::Error::Target(crate::target::Error::Arch(
+        crate::Result::Err(crate::Error::Target(crate::target::Error::Architecture(
             crate::target::arch::Error::X86_64Syscall(
                 crate::target::arch::syscall::Error::X86_64Syscall6(
                     crate::target::arch::syscall::syscall6::Error::Default(errno),
                 ),
             ),
-        ))) => core::result::Result::Err(crate::Error::Target(crate::target::Error::Os(
+        ))) => core::result::Result::Err(crate::Error::Target(crate::target::Error::OperatingSystem(
             crate::target::os::Error::Syscall(crate::target::os::syscall::Error::MMap(
                 Error::Default(errno),
             )),
         ))),
-        _ => core::result::Result::Err(crate::Error::Target(crate::target::Error::Os(
+        _ => core::result::Result::Err(crate::Error::Target(crate::target::Error::OperatingSystem(
             crate::target::os::Error::Syscall(crate::target::os::syscall::Error::MMap(
                 Error::Default(3),
             )),
