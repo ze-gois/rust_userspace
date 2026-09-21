b9ff8f0170457d0b44cb11211b73b880ac5dbbde
Zé Gois <2962558+ze-gois@users.noreply.github.com>
2026-09-21 18:29:48 -0300

    fix: call openat with Linux signature

--- src/file/open.rs
+++ src/file/open.rs
@@ -1,21 +1,23 @@
 pub fn open(file_path: &str) -> isize {
-    let file_path =
-        ample::string::terminate::<crate::memory::heap::Allocator>(file_path);
+    let file_path = ample::string::terminate::<crate::memory::heap::Allocator>(file_path);
 
     if file_path.is_null() {
         return -1;
     }
 
     match crate::target::os::syscall::openat(
-        crate::target::os::syscall::open::AtFlag::FDCWD.to(),
+        crate::target::os::syscall::openat::CURRENT_WORKING_DIRECTORY,
         file_path,
         crate::target::os::syscall::open::Flag::RDONLY.to(),
+        0,
     ) {
-        core::result::Result::Ok(crate::Ok::Target(crate::target::Ok::OperatingSystem(
-            crate::target::os::Ok::Syscall(crate::target::os::syscall::Ok::Open(
-                crate::target::os::syscall::open::Ok::OPENAT(fd),
+        core::result::Result::Ok(crate::Ok::Target(
+            crate::target::Ok::OperatingSystem(crate::target::os::Ok::Syscall(
+                crate::target::os::syscall::Ok::OpenAt(
+                    crate::target::os::syscall::openat::Ok::Default(file_descriptor),
+                ),
             )),
-        ))) => fd as isize,
+        )) => file_descriptor as isize,
         _ => -1,
     }
 }
