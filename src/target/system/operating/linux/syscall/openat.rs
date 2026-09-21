dd95b6da3d6e5c40783370be365556a4175cbddf
Zé Gois <2962558+ze-gois@users.noreply.github.com>
2026-09-21 18:29:19 -0300

    fix: model Linux openat syscall correctly

--- src/target/system/operating/linux/syscall/openat.rs
+++ src/target/system/operating/linux/syscall/openat.rs
@@ -1,7 +1,9 @@
 use crate::target::arch::{Arch, traits::Callable};
 
-pub use crate::target::operating_system::syscall::open::flags;
-pub use crate::target::operating_system::syscall::open::{Error, Ok, Result};
+pub use super::open::{Error, Ok, Result};
+
+pub const CURRENT_WORKING_DIRECTORY: isize = -100;
+pub const AT_FDCWD: isize = CURRENT_WORKING_DIRECTORY;
 
 hooking!(OPENAT);
 
@@ -9,35 +11,36 @@ pub fn openat(
     directory_file_descriptor: isize,
     file_pathname: *const u8,
     flags: i32,
+    mode: i32,
 ) -> crate::Result {
-    let syscall_result = Arch::syscall3(
+    let syscall_result = Arch::syscall4(
         NUMBER,
         directory_file_descriptor as usize,
         file_pathname as usize,
         flags as usize,
+        mode as usize,
     );
 
     handle_result(syscall_result)
 }
 
 pub fn handle_result(result: crate::Result) -> crate::Result {
-    // Err(crate::Error::Default(1))
     match result {
         crate::Result::Ok(crate::Ok::Target(crate::target::Ok::Architecture(
             crate::target::arch::Ok::X86_64Syscall(
-                crate::target::arch::syscall::Ok::X86_64Syscall3(
-                    crate::target::arch::syscall::syscall3::Ok::Default(m),
+                crate::target::arch::syscall::Ok::X86_64Syscall4(
+                    crate::target::arch::syscall::syscall4::Ok::Default(value),
                 ),
             ),
-        ))) => core::result::Result::Ok(crate::Ok::Target(crate::target::Ok::OperatingSystem(
-            crate::target::os::Ok::Syscall(crate::target::os::syscall::Ok::Open(
-                crate::target::os::syscall::open::Ok::OPENAT(m),
+        ))) => core::result::Result::Ok(crate::Ok::Target(
+            crate::target::Ok::OperatingSystem(crate::target::os::Ok::Syscall(
+                crate::target::os::syscall::Ok::OpenAt(Ok::Default(value)),
             )),
-        ))),
-        _ => core::result::Result::Err(crate::Error::Target(crate::target::Error::OperatingSystem(
-            crate::target::os::Error::Syscall(crate::target::os::syscall::Error::Open(
-                Error::Default(3),
+        )),
+        _ => core::result::Result::Err(crate::Error::Target(
+            crate::target::Error::OperatingSystem(crate::target::os::Error::Syscall(
+                crate::target::os::syscall::Error::OpenAt(Error::Default(3)),
             )),
-        ))),
+        )),
     }
 }
