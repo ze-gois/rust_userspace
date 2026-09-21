2a3edee1d2a4f29b0629941dd2468f4b2fa33be5
Zé Gois <2962558+ze-gois@users.noreply.github.com>
2026-09-21 19:14:23 -0300

    refactor: use canonical architecture paths

--- src/target/architecture/x86/bit64/syscall/result.rs
+++ src/target/architecture/x86/bit64/syscall/result.rs
@@ -54,11 +54,11 @@ pub type Result = core::result::Result<Ok, Error>;
 pub fn handle_result(result: usize) -> crate::Result {
     if (result as isize) < 0 {
         core::result::Result::Err(crate::Error::Target(crate::target::Error::Architecture(
-            crate::target::arch::Error::Syscall(Error::from_no(result)),
+            crate::target::architecture::Error::Syscall(Error::from_no(result)),
         )))
     } else {
         core::result::Result::Ok(crate::Ok::Target(crate::target::Ok::Architecture(
-            crate::target::arch::Ok::Syscall(Ok::from_no(result)),
+            crate::target::architecture::Ok::Syscall(Ok::from_no(result)),
         )))
     }
 }
