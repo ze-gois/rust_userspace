696910e07b60ad30a9024dc4580475589e90b06e
Zé Gois <2962558+ze-gois@users.noreply.github.com>
2026-09-21 19:40:41 -0300

    fix: avoid undefined behavior if exit returns

--- src/target/system/operating/linux/syscall/exit.rs
+++ src/target/system/operating/linux/syscall/exit.rs
@@ -4,10 +4,9 @@ use crate::target::architecture::Architecture;
 pub const NUMBER: usize = super::number::x86::bit64::EXIT;
 
 pub fn exit(status_code: i32) -> ! {
-    let status_code = status_code as usize;
+    let _ = Architecture::syscall1(NUMBER, status_code as usize);
 
-    unsafe {
-        let _ = Architecture::syscall1(NUMBER, status_code);
-        core::hint::unreachable_unchecked()
+    loop {
+        core::hint::spin_loop();
     }
 }
