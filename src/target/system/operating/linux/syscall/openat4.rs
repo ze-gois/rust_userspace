e1704f340ed28db222ef1bb342ea74f2a88ee6ed
Zé Gois <2962558+ze-gois@users.noreply.github.com>
2026-09-21 18:27:08 -0300

    refactor: use explicit target result domains

--- src/target/system/operating/linux/syscall/openat4.rs
+++ src/target/system/operating/linux/syscall/openat4.rs
@@ -24,18 +24,18 @@ pub fn openat4(
 pub fn handle_result(result: crate::Result) -> crate::Result {
     // Err(crate::Error::Default(1))
     match result {
-        crate::Result::Ok(crate::Ok::Target(crate::target::Ok::Arch(
+        crate::Result::Ok(crate::Ok::Target(crate::target::Ok::Architecture(
             crate::target::arch::Ok::X86_64Syscall(
                 crate::target::arch::syscall::Ok::X86_64Syscall4(
                     crate::target::arch::syscall::syscall4::Ok::Default(m),
                 ),
             ),
-        ))) => core::result::Result::Ok(crate::Ok::Target(crate::target::Ok::Os(
+        ))) => core::result::Result::Ok(crate::Ok::Target(crate::target::Ok::OperatingSystem(
             crate::target::os::Ok::Syscall(crate::target::os::syscall::Ok::Open(
                 crate::target::os::syscall::open::Ok::OPENAT4(m),
             )),
         ))),
-        _ => core::result::Result::Err(crate::Error::Target(crate::target::Error::Os(
+        _ => core::result::Result::Err(crate::Error::Target(crate::target::Error::OperatingSystem(
             crate::target::os::Error::Syscall(crate::target::os::syscall::Error::Open(
                 Error::Default(3),
             )),
