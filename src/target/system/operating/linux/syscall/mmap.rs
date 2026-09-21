b2fa0b4cefba8a595f96134a802d41896bb34878
Zé Gois <2962558+ze-gois@users.noreply.github.com>
2026-09-21 19:15:18 -0300

    refactor: use canonical Linux paths

--- src/target/system/operating/linux/syscall/mmap.rs
+++ src/target/system/operating/linux/syscall/mmap.rs
@@ -64,8 +64,8 @@ pub fn handle_result(result: crate::Result) -> crate::Result {
                 ),
             ),
         ))) => core::result::Result::Ok(crate::Ok::Target(crate::target::Ok::OperatingSystem(
-            crate::target::os::Ok::Syscall(crate::target::os::syscall::Ok::MMap(
-                crate::target::os::syscall::mmap::Ok::Default(m),
+            crate::target::system::operating::linux::Ok::Syscall(crate::target::system::operating::linux::syscall::Ok::MMap(
+                crate::target::system::operating::linux::syscall::mmap::Ok::Default(m),
             )),
         ))),
         crate::Result::Err(crate::Error::Target(crate::target::Error::Architecture(
@@ -75,12 +75,12 @@ pub fn handle_result(result: crate::Result) -> crate::Result {
                 ),
             ),
         ))) => core::result::Result::Err(crate::Error::Target(crate::target::Error::OperatingSystem(
-            crate::target::os::Error::Syscall(crate::target::os::syscall::Error::MMap(
+            crate::target::system::operating::linux::Error::Syscall(crate::target::system::operating::linux::syscall::Error::MMap(
                 Error::Default(errno),
             )),
         ))),
         _ => core::result::Result::Err(crate::Error::Target(crate::target::Error::OperatingSystem(
-            crate::target::os::Error::Syscall(crate::target::os::syscall::Error::MMap(
+            crate::target::system::operating::linux::Error::Syscall(crate::target::system::operating::linux::syscall::Error::MMap(
                 Error::Default(3),
             )),
         ))),
