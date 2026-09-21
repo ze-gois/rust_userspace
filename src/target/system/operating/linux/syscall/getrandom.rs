060fc61a18cc911fbb649a42d0a29860df024be7
Zé Gois <2962558+ze-gois@users.noreply.github.com>
2026-09-21 19:15:01 -0300

    refactor: use canonical Linux paths

--- src/target/system/operating/linux/syscall/getrandom.rs
+++ src/target/system/operating/linux/syscall/getrandom.rs
@@ -51,12 +51,12 @@ pub fn handle_result(result: crate::Result) -> crate::Result {
                 ),
             ),
         ))) => core::result::Result::Ok(crate::Ok::Target(crate::target::Ok::OperatingSystem(
-            crate::target::os::Ok::Syscall(crate::target::os::syscall::Ok::GetRandom(
-                crate::target::os::syscall::getrandom::Ok::Default(m),
+            crate::target::system::operating::linux::Ok::Syscall(crate::target::system::operating::linux::syscall::Ok::GetRandom(
+                crate::target::system::operating::linux::syscall::getrandom::Ok::Default(m),
             )),
         ))),
         _ => core::result::Result::Err(crate::Error::Target(crate::target::Error::OperatingSystem(
-            crate::target::os::Error::Syscall(crate::target::os::syscall::Error::GetRandom(
+            crate::target::system::operating::linux::Error::Syscall(crate::target::system::operating::linux::syscall::Error::GetRandom(
                 Error::Default(3),
             )),
         ))),
