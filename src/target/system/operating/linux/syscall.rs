784c0baf8ffb9e1323bd244a7a8477a48d091926
Zé Gois <2962558+ze-gois@users.noreply.github.com>
2026-09-21 19:17:02 -0300

    refactor: align Linux syscall identifiers with standards names

--- src/target/system/operating/linux/syscall.rs
+++ src/target/system/operating/linux/syscall.rs
@@ -6,14 +6,14 @@ syscall_modules!(
     [1;   write;    WRITE;    Syscall3;  "Write"],
     [2;   open;     OPEN;     Syscall3;  "Open"],
     [3;   close;    CLOSE;    Syscall1;  "Close"],
-    [5;   fstat;    FSTAT;    Syscall2;  "FStat"],
-    [8;   lseek;    LSEEK;    Syscall3;  "LSeek"],
+    [5;   fstat;    FSTAT;    Syscall2;  "Fstat"],
+    [8;   lseek;    LSEEK;    Syscall3;  "Lseek"],
     [57;  fork;     FORK;     Syscall0;  "Fork"],
     [59;  execve;   EXECVE;   Syscall3;  "Execve"],
-    [9;   mmap;     MMAP;     Syscall6;  "MMap"],
-    [10;  mprotect; MPROTECT; Syscall3;  "MProtect"],
-    [11;  munmap;   MUNMAP;   Syscall2;  "MUnMap"],
+    [9;   mmap;     MMAP;     Syscall6;  "Mmap"],
+    [10;  mprotect; MPROTECT; Syscall3;  "Mprotect"],
+    [11;  munmap;   MUNMAP;   Syscall2;  "Munmap"],
     [60;  exit;     EXIT;     Syscall1;  "Exit"],
-    [257; openat;   OPENAT;   Syscall4;  "OpenAt"],
-    [318; getrandom; GETRANDOM; Syscall3; "GetRandom"]
+    [257; openat;   OPENAT;   Syscall4;  "Openat"],
+    [318; getrandom; GETRANDOM; Syscall3; "Getrandom"]
 );
