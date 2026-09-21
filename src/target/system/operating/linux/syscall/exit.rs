8f1acd7040760dd7caf5e93a1f71d4aeb019ed65
Zé Gois <2962558+ze-gois@users.noreply.github.com>
2026-09-21 20:04:31 -0300

    refactor: make syscall invocation contracts explicit

--- src/target/system/operating/linux/syscall/exit.rs
+++ src/target/system/operating/linux/syscall/exit.rs
@@ -4,7 +4,7 @@ use crate::target::architecture::x86::bit64::syscall;
 pub const NUMBER: usize = super::number::x86::bit64::EXIT;
 
 pub fn exit(status_code: i32) -> ! {
-    let _ = syscall::syscall1(NUMBER, status_code as usize);
+    let _ = unsafe { syscall::syscall1(NUMBER, status_code as usize) };
 
     loop {
         core::hint::spin_loop();
