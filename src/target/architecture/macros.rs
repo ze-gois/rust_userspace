03148b0b0976547b58103d53d42b9e407e33f72b
Zé Gois <2962558+ze-gois@users.noreply.github.com>
2026-09-21 19:40:13 -0300

    refactor: remove obsolete architecture abstraction scaffolding

--- src/target/architecture/macros.rs
+++
@@ -1,9 +0,0 @@
-#[macro_export]
-macro_rules! wrap_syscall {
-    ($name:ident, $syscall:ident, $($arg:ident : $type:ty),*) => {
-        fn $name($($arg: $type,)*) -> $crate::Result {
-            $crate::target::architecture::Architecture::$syscall($($arg),*)
-        }
-    }
-}
-pub use wrap_syscall;
