5ac34075796a0072c2f44ef6a55f7d1148b6bf36
Zé Gois <2962558+ze-gois@users.noreply.github.com>
2026-09-21 19:57:00 -0300

    refactor: keep architecture syscall results raw

--- src/target/architecture/x86/bit64/syscall/syscall3.rs
+++ src/target/architecture/x86/bit64/syscall/syscall3.rs
@@ -1,74 +1,16 @@
-use super::result::*;
-
 #[inline(always)]
-pub fn syscall3(n: usize, a1: usize, a2: usize, a3: usize) -> crate::Result {
-    let syscall_return: usize;
-
+pub fn syscall3(number: usize, argument1: usize, argument2: usize, argument3: usize) -> usize {
+    let result: usize;
     unsafe {
         core::arch::asm!(
             "syscall",
-            inlateout("rax") n => syscall_return,
-            in("rdi") a1,
-            in("rsi") a2,
-            in("rdx") a3,
+            inlateout("rax") number => result,
+            in("rdi") argument1,
+            in("rsi") argument2,
+            in("rdx") argument3,
             out("rcx") _,
             out("r11") _,
         );
     }
-    handle_result(syscall_return)
-}
-
-pub mod ok {
-    ample::result!(
-        Ok;
-        "Architecture syscall Ok";
-        usize;
-        [
-            [0; SYSCALL3_OK; Default; usize; "ZE"; "Entry to ze"],
-        ]
-    );
-
-    impl Ok {
-        pub fn from_no(no: usize) -> Self {
-            Ok::Default(no)
-        }
-    }
-}
-
-pub mod error {
-    ample::result!(
-        Error;
-        "Architecture syscall Error";
-        usize;
-        [
-            [0; SYSCALL3_ERROR; Default; usize; "ZE"; "Entry to ze"],
-        ]
-    );
-
-    impl Error {
-        pub fn from_no(no: usize) -> Self {
-            Error::Default(no)
-        }
-    }
-}
-
-pub use error::Error;
-pub use ok::Ok;
-
-pub type Result = core::result::Result<Ok, Error>;
-
-pub fn handle_result(result: usize) -> crate::Result {
-    if (result as isize) < 0 {
-        core::result::Result::Err(crate::Error::Target(crate::target::Error::Architecture(
-            crate::target::architecture::Error::Syscall(
-                crate::target::architecture::x86::bit64::syscall::Error::Syscall3(Error::Default(result)),
-            ),
-        )))
-    } else {
-        core::result::Result::Ok(crate::Ok::Target(crate::target::Ok::Architecture(
-            crate::target::architecture::Ok::Syscall(
-                crate::target::architecture::x86::bit64::syscall::Ok::Syscall3(Ok::Default(result)),
-            ),
-        )))
-    }
+    result
 }
