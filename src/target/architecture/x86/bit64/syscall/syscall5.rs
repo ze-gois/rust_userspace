6ae40c1cf0cbb575068616ab7629d5cde00c06cf
Zé Gois <2962558+ze-gois@users.noreply.github.com>
2026-09-21 19:11:36 -0300

    refactor: make architecture syscall identifiers semantic

--- src/target/architecture/x86/bit64/syscall/syscall5.rs
+++ src/target/architecture/x86/bit64/syscall/syscall5.rs
@@ -23,10 +23,10 @@ pub fn syscall5(n: usize, a1: usize, a2: usize, a3: usize, a4: usize, a5: usize)
 pub mod ok {
     ample::result!(
         Ok;
-        "Human Ok";
+        "Architecture syscall Ok";
         usize;
         [
-            [0; X86_64_SYSCALL1_OK; Default; usize; "ZE"; "Entry to ze"],
+            [0; SYSCALL5_OK; Default; usize; "ZE"; "Entry to ze"],
         ]
     );
 
@@ -40,10 +40,10 @@ pub mod ok {
 pub mod error {
     ample::result!(
         Error;
-        "Human error";
+        "Architecture syscall Error";
         usize;
         [
-            [0; X86_64_SYSCALL1_ERROR; Default; usize; "ZE"; "Entry to ze"],
+            [0; SYSCALL5_ERROR; Default; usize; "ZE"; "Entry to ze"],
         ]
     );
 
@@ -62,14 +62,14 @@ pub type Result = core::result::Result<Ok, Error>;
 pub fn handle_result(result: usize) -> crate::Result {
     if (result as isize) < 0 {
         core::result::Result::Err(crate::Error::Target(crate::target::Error::Architecture(
-            crate::target::arch::Error::X86_64Syscall(
-                crate::target::arch::syscall::Error::X86_64Syscall5(Error::Default(result)),
+            crate::target::arch::Error::Syscall(
+                crate::target::arch::syscall::Error::Syscall5(Error::Default(result)),
             ),
         )))
     } else {
         core::result::Result::Ok(crate::Ok::Target(crate::target::Ok::Architecture(
-            crate::target::arch::Ok::X86_64Syscall(
-                crate::target::arch::syscall::Ok::X86_64Syscall5(Ok::Default(result)),
+            crate::target::arch::Ok::Syscall(
+                crate::target::arch::syscall::Ok::Syscall5(Ok::Default(result)),
             ),
         )))
     }
