ba28d4cca305d6e451f521df9350d7ff2c37a754
Zé Gois <2962558+ze-gois@users.noreply.github.com>
2026-09-21 19:32:20 -0300

    refactor: keep Linux architecture boundary explicit

--- src/target/system/operating/linux/syscall/write.rs
+++ src/target/system/operating/linux/syscall/write.rs
@@ -56,8 +56,8 @@ pub fn handle_result(result: crate::Result) -> crate::Result {
     match result {
         crate::Result::Ok(crate::Ok::Target(crate::target::Ok::Architecture(
             crate::target::architecture::Ok::Syscall(
-                crate::target::architecture::syscall::Ok::Syscall3(
-                    crate::target::architecture::syscall::syscall3::Ok::Default(m),
+                crate::target::architecture::x86::bit64::syscall::Ok::Syscall3(
+                    crate::target::architecture::x86::bit64::syscall::syscall3::Ok::Default(m),
                 ),
             ),
         ))) => core::result::Result::Ok(crate::Ok::Target(crate::target::Ok::OperatingSystem(
@@ -67,8 +67,8 @@ pub fn handle_result(result: crate::Result) -> crate::Result {
         ))),
         crate::Result::Err(crate::Error::Target(crate::target::Error::Architecture(
             crate::target::architecture::Error::Syscall(
-                crate::target::architecture::syscall::Error::Syscall3(
-                    crate::target::architecture::syscall::syscall3::Error::Default(raw),
+                crate::target::architecture::x86::bit64::syscall::Error::Syscall3(
+                    crate::target::architecture::x86::bit64::syscall::syscall3::Error::Default(raw),
                 ),
             ),
         ))) => core::result::Result::Err(crate::Error::Target(
