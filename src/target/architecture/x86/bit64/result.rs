dd4040a8a4bb120ec735ce981b02b290c2837b4d
Zé Gois <2962558+ze-gois@users.noreply.github.com>
2026-09-21 19:18:37 -0300

    refactor: remove legacy architecture result labels

--- src/target/architecture/x86/bit64/result.rs
+++ src/target/architecture/x86/bit64/result.rs
@@ -1,7 +1,7 @@
 pub mod ok {
     ample::result!( Ok; "Architecture syscall Ok"; usize; [
-        [0; OK; Default; usize; "Ok"; "All good"],
-        [1; X86_64SYSCALL_OK; Syscall; super::super::syscall::Ok; "Ok"; "All good"],
+        [0; OK; Default; usize; "Architecture"; "Architecture syscall result"],
+        [1; SYSCALL_OK; Syscall; super::super::syscall::Ok; "Architecture"; "Architecture syscall result"],
     ]);
 
     impl Ok {
@@ -14,8 +14,8 @@ pub mod ok {
 // diferente
 pub mod error {
     ample::result!(Error; "Architecture syscall Error"; usize; [
-        [1; ERROR; Default; usize; "Error"; "Something wicked this way comes"],
-        [0; OK; Syscall; super::super::syscall::Error; "Ok"; "All good"],
+        [1; ERROR; Default; usize; "Architecture"; "Architecture syscall error"],
+        [0; OK; Syscall; super::super::syscall::Error; "Architecture"; "Architecture syscall result"],
     ]);
 
     impl Error {
