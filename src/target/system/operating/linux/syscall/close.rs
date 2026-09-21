2b0017c22bb3f0c6646c5b2a961ae57304fcc751
Zé Gois <2962558+ze-gois@users.noreply.github.com>
2026-09-21 19:58:05 -0300

    refactor: interpret syscall returns in Linux

--- src/target/system/operating/linux/syscall/close.rs
+++ src/target/system/operating/linux/syscall/close.rs
@@ -5,8 +5,8 @@ pub const NUMBER: usize = super::number::x86::bit64::CLOSE;
 
 #[inline(always)]
 pub fn close(fd: isize) -> crate::Result {
-    let arch_result = syscall::syscall1(NUMBER, fd as usize);
-    handle_result(arch_result)
+    let raw_return = syscall::syscall1(NUMBER, fd as usize);
+    handle_result(raw_return)
 }
 
 pub mod ok {
@@ -39,24 +39,28 @@ pub use ok::Ok;
 
 pub type Result = core::result::Result<Ok, Error>;
 
-pub fn handle_result(result: crate::Result) -> crate::Result {
-    // Err(crate::Error::Default(1))
-    match result {
-        crate::Result::Ok(crate::Ok::Target(crate::target::Ok::Architecture(
-            crate::target::architecture::Ok::Syscall(
-                crate::target::architecture::x86::bit64::syscall::Ok::Syscall1(
-                    crate::target::architecture::x86::bit64::syscall::syscall1::Ok::Default(m),
+pub fn handle_result(raw: usize) -> crate::Result {
+    let result = super::Return::new(raw);
+
+    if result.is_error() {
+        core::result::Result::Err(crate::Error::Target(
+            crate::target::Error::OperatingSystem(
+                crate::target::system::operating::linux::Error::Syscall(
+                    crate::target::system::operating::linux::syscall::Error::Close(
+                        Error::Default(result.raw()),
+                    ),
                 ),
             ),
-        ))) => core::result::Result::Ok(crate::Ok::Target(crate::target::Ok::OperatingSystem(
-            crate::target::system::operating::linux::Ok::Syscall(crate::target::system::operating::linux::syscall::Ok::Close(
-                crate::target::system::operating::linux::syscall::close::Ok::Default(m),
-            )),
-        ))),
-        _ => core::result::Result::Err(crate::Error::Target(crate::target::Error::OperatingSystem(
-            crate::target::system::operating::linux::Error::Syscall(crate::target::system::operating::linux::syscall::Error::Close(
-                Error::Default(3),
-            )),
-        ))),
+        ))
+    } else {
+        core::result::Result::Ok(crate::Ok::Target(
+            crate::target::Ok::OperatingSystem(
+                crate::target::system::operating::linux::Ok::Syscall(
+                    crate::target::system::operating::linux::syscall::Ok::Close(
+                        Ok::Default(result.raw()),
+                    ),
+                ),
+            ),
+        ))
     }
 }
