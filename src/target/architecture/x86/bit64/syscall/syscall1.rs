e9e7ab09354274fdda50636c1ff8fea7d29177bc
Zé Gois <2962558+ze-gois@users.noreply.github.com>
2026-09-21 19:31:21 -0300

    refactor: keep concrete architecture syscall paths explicit

--- src/target/architecture/x86/bit64/syscall/syscall1.rs
+++ src/target/architecture/x86/bit64/syscall/syscall1.rs
@@ -60,13 +60,13 @@ pub fn handle_result(result: usize) -> crate::Result {
     if (result as isize) < 0 {
         core::result::Result::Err(crate::Error::Target(crate::target::Error::Architecture(
             crate::target::architecture::Error::Syscall(
-                crate::target::architecture::syscall::Error::Syscall1(Error::Default(result)),
+                crate::target::architecture::x86::bit64::syscall::Error::Syscall1(Error::Default(result)),
             ),
         )))
     } else {
         core::result::Result::Ok(crate::Ok::Target(crate::target::Ok::Architecture(
             crate::target::architecture::Ok::Syscall(
-                crate::target::architecture::syscall::Ok::Syscall1(Ok::Default(result)),
+                crate::target::architecture::x86::bit64::syscall::Ok::Syscall1(Ok::Default(result)),
             ),
         )))
     }
