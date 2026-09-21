c53ba3a787f8d83f6c8a174f2f2bae16e82934db
Zé Gois <2962558+ze-gois@users.noreply.github.com>
2026-09-21 18:27:32 -0300

    refactor: use explicit target result domains

--- src/target/system/operating/linux/syscall/open.rs
+++ src/target/system/operating/linux/syscall/open.rs
@@ -68,18 +68,18 @@ pub type Result = core::result::Result<Ok, Error>;
 pub fn handle_result(result: crate::Result) -> crate::Result {
     // Err(crate::Error::Default(1))
     match result {
-        crate::Result::Ok(crate::Ok::Target(crate::target::Ok::Arch(
+        crate::Result::Ok(crate::Ok::Target(crate::target::Ok::Architecture(
             crate::target::arch::Ok::X86_64Syscall(
                 crate::target::arch::syscall::Ok::X86_64Syscall3(
                     crate::target::arch::syscall::syscall3::Ok::Default(m),
                 ),
             ),
-        ))) => core::result::Result::Ok(crate::Ok::Target(crate::target::Ok::Os(
+        ))) => core::result::Result::Ok(crate::Ok::Target(crate::target::Ok::OperatingSystem(
             crate::target::os::Ok::Syscall(crate::target::os::syscall::Ok::Open(
                 crate::target::os::syscall::open::Ok::Default(m),
             )),
         ))),
-        _ => core::result::Result::Err(crate::Error::Target(crate::target::Error::Os(
+        _ => core::result::Result::Err(crate::Error::Target(crate::target::Error::OperatingSystem(
             crate::target::os::Error::Syscall(crate::target::os::syscall::Error::Open(
                 Error::Default(3),
             )),
