a3188da09a2302bbf2d460369a40b85563ebb1bf
Zé Gois <2962558+ze-gois@users.noreply.github.com>
2026-09-21 19:54:18 -0300

    refactor: remove empty operating system facade

--- src/target/system/operating.rs
+++ src/target/system/operating.rs
@@ -1,9 +1,5 @@
 #[cfg(target_os = "linux")]
 pub mod linux;
+
 #[cfg(target_os = "linux")]
 pub use linux::{Error, Ok, Result};
-
-#[derive(Debug, Clone, Copy, Default)]
-pub struct OperatingSystem;
-
-pub type Os = OperatingSystem;
