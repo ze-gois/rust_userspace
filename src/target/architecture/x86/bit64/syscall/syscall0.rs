96456e3eb0d03d902fe700dce857f987127eedfa
Zé Gois <2962558+ze-gois@users.noreply.github.com>
2026-09-21 18:26:20 -0300

    refactor: use explicit target result domains

--- src/target/architecture/x86/bit64/syscall/syscall0.rs
+++ src/target/architecture/x86/bit64/syscall/syscall0.rs
@@ -55,13 +55,13 @@ pub type Result = core::result::Result<Ok, Error>;
 
 pub fn handle_result(result: usize) -> crate::Result {
     if (result as isize) < 0 {
-        core::result::Result::Err(crate::Error::Target(crate::target::Error::Arch(
+        core::result::Result::Err(crate::Error::Target(crate::target::Error::Architecture(
             crate::target::arch::Error::X86_64Syscall(
                 crate::target::arch::syscall::Error::X86_64Syscall0(Error::Default(result)),
             ),
         )))
     } else {
-        core::result::Result::Ok(crate::Ok::Target(crate::target::Ok::Arch(
+        core::result::Result::Ok(crate::Ok::Target(crate::target::Ok::Architecture(
             crate::target::arch::Ok::X86_64Syscall(
                 crate::target::arch::syscall::Ok::X86_64Syscall0(Ok::Default(result)),
             ),
