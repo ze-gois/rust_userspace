6badb555613ad72da227acbd3cbf51d18c110ed5
Zé Gois <2962558+ze-gois@users.noreply.github.com>
2026-09-22 16:44:01 -0300

    feat(elf): bind System V hash table to symbols

--- src/file/format/elf/hash.rs
+++ src/file/format/elf/hash.rs
@@ -1,14 +1,45 @@
 //! System V ELF symbol hash table.
 
-#[derive(Debug, Clone, Copy)]
+use ample::r#type::Vec;
+
+use super::symbol_table::SymbolTable;
+
+#[derive(Debug)]
 pub struct HashTable<'file> {
-    pub buckets: &'file [u32],
-    pub chains: &'file [u32],
+    pub buckets: Vec<u32>,
+    pub chains: Vec<u32>,
+    pub symbols: SymbolTable<'file>,
 }
 
 impl<'file> HashTable<'file> {
-    pub const fn new(buckets: &'file [u32], chains: &'file [u32]) -> Self {
-        Self { buckets, chains }
+    pub const fn new(
+        buckets: Vec<u32>,
+        chains: Vec<u32>,
+        symbols: SymbolTable<'file>,
+    ) -> Self {
+        Self {
+            buckets,
+            chains,
+            symbols,
+        }
+    }
+
+    pub fn find(&self, name: &[u8]) -> Option<usize> {
+        if self.buckets.is_empty() {
+            return None;
+        }
+
+        let mut index = *self.buckets.get(hash(name) as usize % self.buckets.len())? as usize;
+
+        while index != 0 {
+            let symbol_name = self.symbols.name(index)?.as_bytes();
+            if symbol_name == name {
+                return Some(index);
+            }
+            index = *self.chains.get(index)? as usize;
+        }
+
+        None
     }
 }
 
