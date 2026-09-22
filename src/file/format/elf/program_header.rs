5d38ad0983fe86edf00232e827473ea113eaa996
Zé Gois <2962558+ze-gois@users.noreply.github.com>
2026-09-22 16:12:14 -0300

    feat(elf): add semantic program and section headers

--- src/file/format/elf/program_header.rs
+++ src/file/format/elf/program_header.rs
@@ -5,3 +5,50 @@
 
 pub mod class_32;
 pub mod class_64;
+pub mod flags;
+pub mod r#type;
+
+pub use flags::Flags;
+pub use r#type::Type;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct ProgramHeader {
+    pub r#type: Type,
+    pub flags: Flags,
+    pub offset: u64,
+    pub virtual_address: u64,
+    pub physical_address: u64,
+    pub file_size: u64,
+    pub memory_size: u64,
+    pub alignment: u64,
+}
+
+impl From<class_32::Representation> for ProgramHeader {
+    fn from(representation: class_32::Representation) -> Self {
+        Self {
+            r#type: Type::from_raw(representation.p_type),
+            flags: Flags::from_raw(representation.p_flags),
+            offset: representation.p_offset as u64,
+            virtual_address: representation.p_vaddr as u64,
+            physical_address: representation.p_paddr as u64,
+            file_size: representation.p_filesz as u64,
+            memory_size: representation.p_memsz as u64,
+            alignment: representation.p_align as u64,
+        }
+    }
+}
+
+impl From<class_64::Representation> for ProgramHeader {
+    fn from(representation: class_64::Representation) -> Self {
+        Self {
+            r#type: Type::from_raw(representation.p_type),
+            flags: Flags::from_raw(representation.p_flags),
+            offset: representation.p_offset,
+            virtual_address: representation.p_vaddr,
+            physical_address: representation.p_paddr,
+            file_size: representation.p_filesz,
+            memory_size: representation.p_memsz,
+            alignment: representation.p_align,
+        }
+    }
+}
