ac071dbdb851606bff4892316c9d1049b046c1f1
Zé Gois <2962558+ze-gois@users.noreply.github.com>
2026-09-22 17:47:25 -0300

    feat(elf): expose dynamic symbol and hash domains

--- src/file/format/elf.rs
+++ src/file/format/elf.rs
@@ -31,3 +31,5 @@ pub mod loadable_segment;
 pub mod program_interpreter;
 pub mod program_header_table_image;
 pub mod thread_local_storage;
+pub mod dynamic_symbol_table;
+pub mod dynamic_hash_table;
