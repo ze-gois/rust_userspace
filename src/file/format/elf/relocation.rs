ce953f309105ec80168766f4c47ebd84d76c5741
Zé Gois <2962558+ze-gois@users.noreply.github.com>
2026-09-22 16:14:24 -0300

    feat(elf): model relocation and dynamic lexicon

---
+++ src/file/format/elf/relocation.rs
@@ -0,0 +1,15 @@
+//! ELF relocation entries.
+
+pub mod class_32;
+pub mod class_64;
+pub mod r#type;
+
+pub use r#type::Type;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Relocation {
+    pub offset: u64,
+    pub symbol_index: u32,
+    pub r#type: Type,
+    pub addend: Option<i64>,
+}
