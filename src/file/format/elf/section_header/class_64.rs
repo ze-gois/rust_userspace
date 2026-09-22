01c180c355de34e6ef42c3b92718d71a3120d740
Zé Gois <2962558+ze-gois@users.noreply.github.com>
2026-09-22 18:29:04 -0300

    feat(elf): decode section and symbol representations by EI_DATA

--- src/file/format/elf/section_header/class_64.rs
+++ src/file/format/elf/section_header/class_64.rs
@@ -1,4 +1,7 @@
-use super::super::representation::class_64 as representation;
+use super::super::{
+    identification::Data,
+    representation::{class_64 as representation, Decoder},
+};
 
 #[repr(C)]
 #[derive(Debug, Clone, Copy)]
@@ -13,4 +16,22 @@ pub struct Representation {
     pub sh_info: representation::Word,
     pub sh_addralign: representation::Xword,
     pub sh_entsize: representation::Xword,
+}
+
+impl Representation {
+    pub fn decode(bytes: &[u8], offset: usize, data: Data) -> Option<Self> {
+        let mut decoder = Decoder::new(bytes, offset, data)?;
+        Some(Self {
+            sh_name: decoder.word()?,
+            sh_type: decoder.word()?,
+            sh_flags: decoder.xword()?,
+            sh_addr: decoder.xword()?,
+            sh_offset: decoder.xword()?,
+            sh_size: decoder.xword()?,
+            sh_link: decoder.word()?,
+            sh_info: decoder.word()?,
+            sh_addralign: decoder.xword()?,
+            sh_entsize: decoder.xword()?,
+        })
+    }
 }
