8bec5a8ccda7f0f3ba326b199ac7a05c62ae40d6
Zé Gois <2962558+ze-gois@users.noreply.github.com>
2026-09-22 19:30:46 -0300

    feat(elf): recognize Zstandard compression

--- src/file/format/elf/compression/type.rs
+++ src/file/format/elf/compression/type.rs
@@ -3,6 +3,7 @@
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Type {
     Zlib,
+    Zstandard,
     OperatingSystemSpecific(u32),
     ProcessorSpecific(u32),
     Reserved(u32),
@@ -12,6 +13,7 @@ impl Type {
     pub const fn from_raw(raw: u32) -> Self {
         match raw {
             1 => Self::Zlib,
+            2 => Self::Zstandard,
             0x6000_0000..=0x6fff_ffff => Self::OperatingSystemSpecific(raw),
             0x7000_0000..=0x7fff_ffff => Self::ProcessorSpecific(raw),
             _ => Self::Reserved(raw),
@@ -21,6 +23,7 @@ impl Type {
     pub const fn raw(self) -> u32 {
         match self {
             Self::Zlib => 1,
+            Self::Zstandard => 2,
             Self::OperatingSystemSpecific(raw)
             | Self::ProcessorSpecific(raw)
             | Self::Reserved(raw) => raw,
