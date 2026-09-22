aa388e8cc8e505b8a3b48d40e1068596a0df9d41
Zé Gois <2962558+ze-gois@users.noreply.github.com>
2026-09-22 16:44:05 -0300

    feat(elf): model section group relation

---
+++ src/file/format/elf/section_group.rs
@@ -0,0 +1,54 @@
+//! ELF section group.
+
+use ample::r#type::Vec;
+
+use super::symbol_table::SymbolTable;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Default)]
+pub struct Flags(u32);
+
+impl Flags {
+    pub const COMDAT: u32 = 0x1;
+    pub const OPERATING_SYSTEM_MASK: u32 = 0x0ff0_0000;
+    pub const PROCESSOR_MASK: u32 = 0xf000_0000;
+
+    pub const fn from_raw(raw: u32) -> Self {
+        Self(raw)
+    }
+
+    pub const fn raw(self) -> u32 {
+        self.0
+    }
+
+    pub const fn comdat(self) -> bool {
+        self.0 & Self::COMDAT != 0
+    }
+}
+
+#[derive(Debug)]
+pub struct SectionGroup<'file> {
+    pub flags: Flags,
+    pub members: Vec<usize>,
+    pub symbols: SymbolTable<'file>,
+    pub signature_symbol_index: usize,
+}
+
+impl<'file> SectionGroup<'file> {
+    pub const fn new(
+        flags: Flags,
+        members: Vec<usize>,
+        symbols: SymbolTable<'file>,
+        signature_symbol_index: usize,
+    ) -> Self {
+        Self {
+            flags,
+            members,
+            symbols,
+            signature_symbol_index,
+        }
+    }
+
+    pub fn signature_name(&self) -> Option<&'file str> {
+        self.symbols.name(self.signature_symbol_index)
+    }
+}
