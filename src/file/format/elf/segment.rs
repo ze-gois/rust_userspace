6031dba50eb3089a910fd5a2e82aec2312d1d8f9
Zé Gois <2962558+ze-gois@users.noreply.github.com>
2026-09-22 16:12:32 -0300

    feat(elf): model sections segments and string tables

---
+++ src/file/format/elf/segment.rs
@@ -0,0 +1,30 @@
+//! ELF segment contents.
+
+use super::program_header::ProgramHeader;
+
+#[derive(Debug, Clone, Copy)]
+pub struct Segment<'file> {
+    pub program_header: ProgramHeader,
+    pub file_image: &'file [u8],
+}
+
+impl<'file> Segment<'file> {
+    pub const fn new(program_header: ProgramHeader, file_image: &'file [u8]) -> Self {
+        Self {
+            program_header,
+            file_image,
+        }
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.file_image.is_empty()
+    }
+
+    pub fn len(&self) -> usize {
+        self.file_image.len()
+    }
+
+    pub const fn memory_size(&self) -> u64 {
+        self.program_header.memory_size
+    }
+}
