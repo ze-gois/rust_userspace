c9d745725ffb287bdd606397fe2688a9536cfb1f
Zé Gois <2962558+ze-gois@users.noreply.github.com>
2026-09-22 19:01:03 -0300

    feat(elf): expose dynamic state flags

--- src/file/format/elf/dynamic.rs
+++ src/file/format/elf/dynamic.rs
@@ -2,10 +2,12 @@
 
 pub mod class_32;
 pub mod class_64;
+pub mod flags;
 pub mod section;
 pub mod tag;
 pub mod validation;
 
+pub use flags::Flags;
 pub use tag::{PayloadKind, Tag};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
