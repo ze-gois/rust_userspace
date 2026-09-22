3b4d222130190aa0efb3cabe873d85cdbbebd701
Zé Gois <2962558+ze-gois@users.noreply.github.com>
2026-09-22 16:09:00 -0300

    feat(elf): establish gABI representation lexicon

---
+++ src/file/format/elf/program_header.rs
@@ -0,0 +1,7 @@
+//! Program header table entries.
+//!
+//! A program header entry describes a segment or other information required
+//! to prepare a program for execution. It is not itself the segment.
+
+pub mod class_32;
+pub mod class_64;
