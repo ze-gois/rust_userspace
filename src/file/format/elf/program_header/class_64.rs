99f81a7b0ed73fcb5cad459448b67208dd5b13c4
Zé Gois <2962558+ze-gois@users.noreply.github.com>
2026-09-22 18:28:41 -0300

    feat(elf): decode headers by ELF byte order

--- src/file/format/elf/program_header/class_64.rs
+++ src/file/format/elf/program_header/class_64.rs
@@ -1,4 +1,7 @@
-use super::super::representation::class_64 as representation;
+use super::super::{
+    identification::Data,
+    representation::{class_64 as representation, Decoder},
+};
 
 #[repr(C)]
 #[derive(Debug, Clone, Copy)]
@@ -11,4 +14,20 @@ pub struct Representation {
     pub p_filesz: representation::Xword,
     pub p_memsz: representation::Xword,
     pub p_align: representation::Xword,
+}
+
+impl Representation {
+    pub fn decode(bytes: &[u8], offset: usize, data: Data) -> Option<Self> {
+        let mut decoder = Decoder::new(bytes, offset, data)?;
+        Some(Self {
+            p_type: decoder.word()?,
+            p_flags: decoder.word()?,
+            p_offset: decoder.xword()?,
+            p_vaddr: decoder.xword()?,
+            p_paddr: decoder.xword()?,
+            p_filesz: decoder.xword()?,
+            p_memsz: decoder.xword()?,
+            p_align: decoder.xword()?,
+        })
+    }
 }
