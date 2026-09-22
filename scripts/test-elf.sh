18d4ecea34f2c57de99f230827bc4e63636006a2
Zé Gois <2962558+ze-gois@users.noreply.github.com>
2026-09-22 19:43:19 -0300

    test(elf): compile host inspection example

--- scripts/test-elf.sh
+++ scripts/test-elf.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+cargo check \
+  -Z build-std \
+  --target x86_64-unknown-linux-gnu \
+  --no-default-features \
+  --features host_tests,with_std \
+  --example elf_inspection
+
 cargo test   -Z build-std   --target x86_64-unknown-linux-gnu   --no-default-features   --features host_tests,with_std   --test elf_representation   --test elf_dynamic_initialization   --test elf_dynamic_flags   --test elf_relative_relocation   --test elf_dynamic_symbol_table_size   --test elf_dynamic_relative_relocation   --test elf_section_relative_relocation   --test elf_symbol_visibility   --test elf_section_link_order   --test elf_compression
