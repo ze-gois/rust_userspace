6ce3dc084221f7f5a2000f9bd2f9ae5386667433
Zé Gois <2962558+ze-gois@users.noreply.github.com>
2026-09-22 22:48:40 -0300

    fix(elf): forbid allocated compressed sections

--- src/file/format/elf/compression.rs
+++ src/file/format/elf/compression.rs
@@ -11,7 +11,7 @@ pub use r#type::Type;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum ValidationError {
-    AllocatedCompressedSectionInExecutableOrSharedObject,
+    AllocatedCompressedSection,
     NoBitsCompressedSection,
     MissingCompressionHeader,
     UncompressedAlignmentNotPowerOfTwo,
