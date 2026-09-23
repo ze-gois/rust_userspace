72a25ca01ade26a42cbfe376e17febc754a2c98e
Zé Gois <2962558+ze-gois@users.noreply.github.com>
2026-09-23 17:47:41 -0300

    refactor(target): name target domains explicitly

--- src/target/operating_system.rs
+++ src/target/operating_system.rs
@@ -7,9 +7,6 @@ pub mod linux;
 #[cfg(target_os = "linux")]
 pub use linux::*;
 
-#[cfg(not(target_os = "linux"))]
-pub mod linux;
-#[cfg(not(target_os = "linux"))]
-pub use linux::*;
+pub struct OperatingSystem;
 
-pub struct Os;
+pub type Os = OperatingSystem;
