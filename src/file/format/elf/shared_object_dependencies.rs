aa1d46100a95943a2eae5752e6a1cfa310d668a7
Zé Gois <2962558+ze-gois@users.noreply.github.com>
2026-09-23 16:46:03 -0300

    feat(elf): represent needed shared object dependencies

--- src/file/format/elf/shared_object_dependencies.rs
+++ src/file/format/elf/shared_object_dependencies.rs
@@ -2,9 +2,24 @@
 
 use ample::r#type::Vec;
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct SharedObjectDependency<'file> {
+    pub dynamic_entry_index: usize,
+    pub name: &'file str,
+}
+
+impl<'file> SharedObjectDependency<'file> {
+    pub const fn new(dynamic_entry_index: usize, name: &'file str) -> Self {
+        Self {
+            dynamic_entry_index,
+            name,
+        }
+    }
+}
+
 #[derive(Debug)]
 pub struct SharedObjectDependencies<'file> {
-    pub needed: Vec<&'file str>,
+    pub needed: Vec<SharedObjectDependency<'file>>,
     pub shared_object_name: Option<&'file str>,
     pub runtime_search_path: Option<&'file str>,
     pub run_path: Option<&'file str>,
@@ -12,7 +27,7 @@ pub struct SharedObjectDependencies<'file> {
 
 impl<'file> SharedObjectDependencies<'file> {
     pub const fn new(
-        needed: Vec<&'file str>,
+        needed: Vec<SharedObjectDependency<'file>>,
         shared_object_name: Option<&'file str>,
         runtime_search_path: Option<&'file str>,
         run_path: Option<&'file str>,
